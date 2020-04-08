61ba808b07bf156cf16efad2d90693f4040f9639
nicanor-alicaya <donalicaya@gmail.com>
2020-04-08 19:28:41 -0400

    This assignment is like wizardry! So far so good.

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -38,14 +39,42 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-
+player = Player(room['outside'])
 # Write a loop that:
-#
-# * Prints the current room name
+while True:
+    #
+    # * Prints the current room name
+    print(f"Current Room: {player.room.name}")
 # * Prints the current description (the textwrap module might be useful here).
+    print(f"{player.room.description}")
 # * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
+    move = input("[w] North [s] South [a] East [d] West [q] Quit:\n")
+
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+    if move == 'q':
+        break
+# If the user enters a cardinal direction, attempt to move to the room there.
+    elif move == 'w':
+        if player.room.n_to != None:
+            player.room = player.room.n_to
+        else:
+            print("Something blocks your way, you can't move North")
+    elif move == 's':
+        if player.room.s_to != None:
+            player.room = player.room.s_to
+        else:
+            print("Something blocks your way, you can't move South")
+    elif move == 'd':
+        if player.room.e_to != None:
+            player.room = player.room.e_to
+        else:
+            print("Something blocks your way, you can't move East")
+    elif move == 'a':
+        if player.room.w_to != None:
+            player.room = player.room.w_to
+        else:
+            print("Something blocks your way, you can't move West")
+    else:
+        print("Invalid movement. Press W for North, S for South, A for West, D for East or Q to Quit game")
--- src/player.py
+++ src/player.py
@@ -1,2 +1,9 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player():
+    '''docstring for Player'''
+
+    def __init__(self, room):
+        self.room = room
